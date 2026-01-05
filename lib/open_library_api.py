0063b7692496a9d4754bdf6d7f3e1f6d2414dff6
habiba hussein <habiba.hussein@moringaschool.comclear>
2026-01-05 19:57:22 +0300

    working with APIs

--- lib/open_library_api.py
+++ lib/open_library_api.py
@@ -9,14 +9,17 @@ class Search:
 
         search_term_formatted = search_term.replace(" ", "+")
         fields = ["title", "author_name"]
-        # formats the list into a comma separated string
-        # output: "title,author_name"
         fields_formatted = ",".join(fields)
         limit = 1
 
-        URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
+        url = (
+            f"https://openlibrary.org/search.json"
+            f"?title={search_term_formatted}"
+            f"&fields={fields_formatted}"
+            f"&limit={limit}"
+        )
 
-        response = requests.get(URL)
+        response = requests.get(url)
         return response.content
 
     def get_search_results_json(self):
@@ -27,9 +30,14 @@ class Search:
         fields_formatted = ",".join(fields)
         limit = 1
 
-        URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
-        print(URL)
-        response = requests.get(URL)
+        url = (
+            f"https://openlibrary.org/search.json"
+            f"?title={search_term_formatted}"
+            f"&fields={fields_formatted}"
+            f"&limit={limit}"
+        )
+
+        response = requests.get(url)
         return response.json()
 
     def get_user_search_results(self, search_term):
@@ -38,20 +46,15 @@ class Search:
         fields_formatted = ",".join(fields)
         limit = 1
 
-        URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
+        url = (
+            f"https://openlibrary.org/search.json"
+            f"?title={search_term_formatted}"
+            f"&fields={fields_formatted}"
+            f"&limit={limit}"
+        )
 
-        response = requests.get(URL).json()
-        response_formatted = f"Title: {response['docs'][0]['title']}\nAuthor: {response['docs'][0]['author_name'][0]}"
-        return response_formatted
-
-
-# results = Search().get_search_results()
-# print(results)
-
-# results_json = Search().get_search_results_json()
-# print(json.dumps(results_json, indent=1))
-
-search_term = input("Enter a book title: ")
-result = Search().get_user_search_results(search_term)
-print("Search Result:\n")
-print(result)
+        response = requests.get(url).json()
+        return (
+            f"Title: {response['docs'][0]['title']}\n"
+            f"Author: {response['docs'][0]['author_name'][0]}"
+        )
